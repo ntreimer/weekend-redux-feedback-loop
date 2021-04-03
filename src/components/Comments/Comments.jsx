ff96e8778390b1ec5e3944c69780ace374deb123
Nate Treimer <natetreimer@gmail.com>
2021-04-03 14:33:57 -0500

    dispatch working for all components

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {useHistory} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
 
 function Comments() {
     const history = useHistory();
+    const dispatch = useDispatch();
+    const [comments, setComments] = useState('');
+    const userComments = (event) => {
+        setComments(event.target.value)
+    }
     const goToReview = () => {
         history.push('/review');
+        dispatch({type: 'SET_COMMENTS', payload: comments});
     }
     return (<>
         <h2>Any comments you want to leave?</h2>
-        <input type="text"/>
+        <input type="text" onChange={userComments}/>
         <button type="button" onClick={goToReview}>Next</button>
     </>);
 }
--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {useHistory} from 'react-router-dom';
 
 function Support() {
     const history = useHistory();
+    const dispatch = useDispatch();
+    const [support, setSupport] = useState('');
+    const userSupport = (event) => {
+        setSupport(event.target.value);
+        dispatch({
+            type: 'SET_SUPPORT', 
+            payload: event.target.value})
+    }
     const goToComments = () => {
-        history.push('/comments');
+        if (support !== '') {
+            history.push('/comments');
+        }
+        else {
+            alert('Please select a rating.')
+        }
     }
     return (<>
         <h2>How well are you being supported?</h2>
-        <select name="support-rating" id="support-rating">
-            <option value="" disabled selected>Select a rating</option>
+        <select name="support-rating" id="support-rating" defaultValue={'DEFAULT'} onChange={userSupport}>
+            <option value="DEFAULT" disabled>Choose a rating</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
