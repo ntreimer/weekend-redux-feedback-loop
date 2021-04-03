d53532a87c76d92296fa2ef2b4783d64748a05c0
Nate Treimer <natetreimer@gmail.com>
2021-04-03 13:30:49 -0500

    useSelector on Review component to retrieve feelings from store

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 function Review() {
     const history = useHistory();
     const goToThanks = () => {
         history.push('/thanks');
     }
+    const feelings = useSelector((store) => {
+        return store.feelingRating;
+    })
     return (<>
         <h2>Is this the feedback you wish to submit?</h2>
-        <button type="button" onClick={goToThanks}>Next</button>
+        <ul>
+            <li>Feelings: {feelings}</li>
+        </ul>
+        <button type="button" onClick={goToThanks}>Submit</button>
     </>);
 }
 
