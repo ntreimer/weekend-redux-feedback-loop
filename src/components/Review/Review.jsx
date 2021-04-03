952a3fe29d86347feaa133ff0ff7e36ff167f635
Nate Treimer <natetreimer@gmail.com>
2021-04-03 14:45:56 -0500

    useSelector to show comments, support, understanding on review page

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -7,13 +7,25 @@ function Review() {
     const goToThanks = () => {
         history.push('/thanks');
     }
+    const comments = useSelector((store) => {
+        return store.comments;
+    })
     const feelings = useSelector((store) => {
         return store.feelingRating;
+    })
+    const support = useSelector((store) => {
+        return store.supportRating;
+    })
+    const understanding = useSelector((store) => {
+        return store.understandingRating;
     })
     return (<>
         <h2>Is this the feedback you wish to submit?</h2>
         <ul>
             <li>Feelings: {feelings}</li>
+            <li>Understanding: {understanding}</li>
+            <li>Support: {support}</li>
+            <li>Comments: {comments}</li>
         </ul>
         <button type="button" onClick={goToThanks}>Submit</button>
     </>);
